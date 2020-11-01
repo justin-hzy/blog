04b87fa1067ac0e12018a56d3527df6d79c7f498
justin-hzy <771736395@qq.com>
2020-11-01 12:25:18 +0800

    页面初步完成注销功能，登陆模块加上md5加密功能

---
+++ blog-commons-3001/src/main/java/com/eva/utils/MD5Code.java
@@ -0,0 +1,34 @@
+package com.eva.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+//md5加密工具类
+public class MD5Code {
+    public static String code(String str){
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(str.getBytes());
+            byte[] bytes = md.digest();
+            int i;
+            StringBuffer buf = new StringBuffer();
+            for (int offset = 0;offset<bytes.length;offset++){
+                i = bytes[offset];
+                if (i<0){
+                    i+=256;
+                }
+                if (i<16){
+                    buf.append("0");
+                }
+                buf.append(Integer.toHexString(i));
+            }
+            //32位
+            return buf.toString();
+            //64位
+            // return buf.toString().substring(8,24);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
