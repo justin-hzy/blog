e5e9bbd05b0f6643ae6286d8d06a5c1db37d41ab
justin-hzy <771736395@qq.com>
2020-11-10 14:21:24 +0800

    dto实体删除自动生成equal方法、hascode方法

--- blog-dto-4001/src/main/java/com/eva/dto/Tag.java
+++ blog-dto-4001/src/main/java/com/eva/dto/Tag.java
@@ -30,31 +30,6 @@ public class Tag implements Serializable {
     }
 
     @Override
-    public boolean equals(Object that) {
-        if (this == that) {
-            return true;
-        }
-        if (that == null) {
-            return false;
-        }
-        if (getClass() != that.getClass()) {
-            return false;
-        }
-        Tag other = (Tag) that;
-        return (this.getTagId() == null ? other.getTagId() == null : this.getTagId().equals(other.getTagId()))
-            && (this.getTagName() == null ? other.getTagName() == null : this.getTagName().equals(other.getTagName()));
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((getTagId() == null) ? 0 : getTagId().hashCode());
-        result = prime * result + ((getTagName() == null) ? 0 : getTagName().hashCode());
-        return result;
-    }
-
-    @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append(getClass().getSimpleName());
--- blog-dto-4001/src/main/java/com/eva/dto/TagBlogRelation.java
+++ blog-dto-4001/src/main/java/com/eva/dto/TagBlogRelation.java
@@ -30,31 +30,6 @@ public class TagBlogRelation implements Serializable {
     }
 
     @Override
-    public boolean equals(Object that) {
-        if (this == that) {
-            return true;
-        }
-        if (that == null) {
-            return false;
-        }
-        if (getClass() != that.getClass()) {
-            return false;
-        }
-        TagBlogRelation other = (TagBlogRelation) that;
-        return (this.getTagId() == null ? other.getTagId() == null : this.getTagId().equals(other.getTagId()))
-            && (this.getBlogId() == null ? other.getBlogId() == null : this.getBlogId().equals(other.getBlogId()));
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((getTagId() == null) ? 0 : getTagId().hashCode());
-        result = prime * result + ((getBlogId() == null) ? 0 : getBlogId().hashCode());
-        return result;
-    }
-
-    @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append(getClass().getSimpleName());
